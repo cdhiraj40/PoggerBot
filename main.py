739e0370aba9c504187540ed141b5ee62368f4c5
TejasNair9977 <tejasnair2002@gmail.com>
2021-10-07 10:29:25 +0530

    added music feature

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import requests
 import json
 import random
 from keep_alive import keep_alive
+from discord.ext import commands
+from dotenv import load_dotenv
+import youtube_dl
 
-
-
-client = discord.Client()
-
+load_dotenv()
+bot = commands.Bot(command_prefix=',')
 
 cheesy_pick_up_lines=[
     "hey baby are you allergic to dairy cause I laktose clothes you're wearing",
@@ -172,14 +173,14 @@ def get_quote():
   return(quote)
 
     
-@client.event
+@bot.event
 async def on_ready():
-    print(' We have logged in as {0.user}'.format(client))
+    print(' We have logged in as {0.user}'.format(bot))
 
     
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
 
     if message.content.startswith('weeb_scanner'):
@@ -205,7 +206,7 @@ async def on_message(message):
         if robama.endswith(".jpg"):
             await message.channel.send(file=discord.File(robama))
         else:
-            await message.channel.send(random.choice(obama))
+            await message.channel.send(robama)
 
     if message.content.startswith('waifus'):
         await message.channel.send(random.choice(anime_waifus))
@@ -275,11 +276,107 @@ async def on_message(message):
 
     if message.content.startswith('shut up'):
         await message.channel.send("oh you should'nt fight me")
-        
+    await bot.process_commands(message)
 
-        
-    
-        
-keep_alive()
-client.run(os.getenv('TOKEN'))
 
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='play', help='To play song')
+async def play(ctx, url):
+    try:
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        await ctx.send('**Now playing:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@bot.command(name='pause', help='This command pauses the song')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.command(name='resume', help='Resumes the song')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use play command")
+
+
+@bot.command(name='stop', help='Stops the song')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+if __name__ == "__main__":
+    keep_alive()
+    bot.run(os.getenv('TOKEN'))
